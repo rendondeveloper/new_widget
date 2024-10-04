4c23b5a26b01be14534582a7e939b2bc935efb96
luis rendon <luis.rendon@aplazo.mx>
2024-10-04 09:43:12 -0600

    update fles

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,13 +1,7 @@
-import 'dart:io';
+
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  final filePath = 'example/lib/home_page.dart';
-  final file = File(filePath);
 
-  if (file.existsSync()) {
-    context.logger.info('home_page.dart has been updated.');
-  } else {
-    context.logger.err('home_page.dart not found.');
-  }
+  context.logger.info('pre gen log');
 }
